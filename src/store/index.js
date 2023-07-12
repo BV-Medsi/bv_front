09f94905db6e12d6135b44f720878bcdc426d447
Andrew <a@fandb.pro>
2023-07-12 22:30:40 +0300

    feat(image-step): move data to sybling directory

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import {defineStore} from 'pinia'
 import {ROUTES} from "../router/index.js";
+import {symptomsData} from "./data/index.js";
 
 export const useStore = defineStore({
     id: "therapist-store",
@@ -27,186 +28,31 @@ export const useStore = defineStore({
                 data: {
                     head: {
                         isSelected: false,
-                        symptoms: [
-                            {
-                                name: 'Головная боль',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Мигрень',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Выпадение волос',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Першение в горле',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Шум в ушах',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Снижение зрения',
-                                isChecked: false
-                            }
-                        ]
+                        symptoms: [...symptomsData.head]
                     },
                     chest: {
                         isSelected: false,
-                        symptoms: [
-                            {
-                                name: 'Кашель',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Перебой в работе сердца',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Нехватка воздуха',
-                                isChecked: false
-                            }
-                        ]
+                        symptoms: [...symptomsData.chest]
                     },
                     arms: {
                         isSelected: false,
-                        symptoms: [
-                            {
-                                name: 'Подергивание руки',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Отек руки',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Рука онемела',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Боль в руках при движении',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Дрожь руки',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Рука посинела',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Холодная рука',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Слабость в руке',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Не могу двигать рукой',
-                                isChecked: false
-                            }
-                        ]
+                        symptoms: [...symptomsData.arms]
                     },
                     stomach: {
                         isSelected: false,
-                        symptoms: [
-                            {
-                                name: 'Боль в животе',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Вздутие живота',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Урчание',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Диарея',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Изжога',
-                                isChecked: false
-                            }
-                        ]
+                        symptoms: [...symptomsData.stomach]
                     },
                     back: {
                         isSelected: false,
-                        symptoms: [
-                            {
-                                name: 'Боль в пояснице',
-                                isChecked: false
-                            }
-                        ]
+                        symptoms: [...symptomsData.back]
                     },
                     buttocks: {
                         isSelected: false,
-                        symptoms: [
-                            {
-                                name: 'кровь в кале',
-                                isChecked: false
-                            }
-                        ]
+                        symptoms: [...symptomsData.buttock]
                     },
                     legs: {
                         isSelected: false,
-                        symptoms: [
-                            {
-                                name: 'Боль в ноге',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Нога онемела',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Синяк на ноге',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Боль в колене',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Боль в пятке',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Боль в пальцах ноги',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Боль в икрах ног',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Не могу двигать ногой',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Нога посинела',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Слабость в мышцах',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Больно двигать ногой',
-                                isChecked: false
-                            },
-                            {
-                                name: 'Нога покраснела',
-                                isChecked: false
-                            }
-                        ]
+                        symptoms: [...symptomsData.legs]
                     }
                 },
                 isValid: false
@@ -214,28 +60,7 @@ export const useStore = defineStore({
             {
                 id: ROUTES.SYMPTOMS,
                 question: "Выберите ваши симптомы",
-                data: [
-                    {
-                        isChecked: false,
-                        name: 'Глубокая боль'
-                    },
-                    {
-                        isChecked: false,
-                        name: 'Кожная боль'
-                    },
-                    {
-                        isChecked: false,
-                        name: 'Нейропатическая боль'
-                    },
-                    {
-                        isChecked: false,
-                        name: 'Острая боль'
-                    },
-                    {
-                        isChecked: false,
-                        name: 'Рефлекторная боль'
-                    }
-                ],
+                data: [...symptomsData.other],
                 isValid: false
             }
         ],
