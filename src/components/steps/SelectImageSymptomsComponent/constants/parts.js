f66122f504e6f788e402497342a141a1ce440c96
Andrew <a@fandb.pro>
2023-07-14 20:13:57 +0300

    feat(image-step): add part selection on image front

---
+++ src/components/steps/SelectImageSymptomsComponent/constants/parts.js
@@ -0,0 +1,11 @@
+export const PARTS = {
+    head: 'head',
+    neck: 'neck',
+    chest: 'chest',
+    arms: 'arms',
+    stomach:'stomach',
+    pelvis:'pelvis',
+    back: 'back',
+    buttock: 'buttock',
+    legs: 'legs'
+}
