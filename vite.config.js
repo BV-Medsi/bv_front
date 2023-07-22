2a1046207530e20b539393e611fd745699519c8a
amirh <amirhazeev60189@gmail.com>
2023-07-22 11:25:35 +0300

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: "https://rollupjs.org/configuration-options/#external"
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
