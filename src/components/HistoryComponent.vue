da0f0c6e3d5d1c499ee9ff9832a9e8ad4bfbfa65
amirh <amirhazeev60189@gmail.com>
2023-07-23 09:11:56 +0300

    creating history screen

--- src/components/HistoryComponent.vue
+++ src/components/HistoryComponent.vue
@@ -4,77 +4,127 @@ import Spinner from "@smartmed/ui/Spinner";
 import BaseButton from "@smartmed/ui/BaseButton";
 import {useHistoryStore} from '../store/history';
 import {storeToRefs} from "pinia";
-import {onMounted, useCssModule} from "vue";
+import {computed, onMounted, useCssModule} from "vue";
 
 const historyStore = useHistoryStore();
 
-const {isLoading, history} = storeToRefs(historyStore);
+const {isLoading, history, indicators} = storeToRefs(historyStore);
 const {fetchHistory, loadPreviousHistoryItem} = historyStore;
-onMounted(() => {
-  fetchHistory()
+
+onMounted(async () => {
+  await fetchHistory()
+  sortDoctors(history.answer)
 });
-const s = useCssModule();
-const generateHTML = (obj) => {
-  let html = `<ul class="${s.list}">`;
-  for (let key in obj) {
-    if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
-      html += `<li><span>${key}:</span> ${generateHTML(obj[key])}</li>`;
-    } else if (Array.isArray(obj[key])) {
-      html += `<li><span>${key}:</span> <ul>${obj[key].map(item => typeof item === 'object' ? generateHTML(item) : `<li>${item}</li>`).join('')}</ul></li>`;
-    } else {
-      html += `<li>${key}: ${obj[key]}</li>`;
-    }
+
+const translate = {
+  temperature: "Температура",
+  weight: 'Вес',
+  growth: 'Рост',
+  age: 'Возраст',
+  gender: 'Пол',
+  diseases: 'Заболевания',
+  chronic_diseases: 'Хронические заболевания'
+}
+function func(name, value){
+  if(translate[name] !== undefined){
+    return translate[name] + ' ' + value;
   }
-  html += '</ul><hr />';
-  return html;
 }
 
-const objectData = {
-  card: {
-    id: "UUID",
-    age: 30,
-    gender: 1,
-    operations: ["value1", "value2"],
-    diseases: ["value1", "value2"],
-    chronic_diseases: ["value1", "value2"]
-  },
-  complains: {
-    head: ["value1", "value2"],
-    leg: "value"
-  },
-  indicators: {
-    temperature: 36.6,
-    pressure: 1.56,
-    growth: 178,
-    weight: 80.78,
-    sugar: 0.8,
-    oxygen: 100.1
-  },
-  answer: [
-    {doctor: 0.1},
-    {doctor: 0.09},
-    {doctor: 0.08}
-  ]
+function sortDoctors(data){
+  let doctors_list = [];
+  for(let i in data){
+    doctors_list.push(i);
+  }
+  console.log(doctors_list);
 }
+
+//const s = useCssModule();
+// const generateHTML = (obj) => {
+//   let html = `<ul class="${s.list}">`;
+//   for (let key in obj) {
+//     if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+//       html += `<li><span>${key}:</span> ${generateHTML(obj[key])}</li>`;
+//     } else if (Array.isArray(obj[key])) {
+//       html += `<li><span>${key}:</span> <ul>${obj[key].map(item => typeof item === 'object' ? generateHTML(item) : `<li>${item}</li>`).join('')}</ul></li>`;
+//     } else {
+//       html += `<li>${key}: ${obj[key]}</li>`;
+//     }
+//   }
+//   html += '</ul><hr />';
+//   return html;
+// }
+//
+// const objectData = {
+//   card: {
+//     id: "UUID",
+//     age: 30,
+//     gender: 1,
+//     operations: ["value1", "value2"],
+//     diseases: ["value1", "value2"],
+//     chronic_diseases: ["value1", "value2"]
+//   },
+//   complains: {
+//     head: ["value1", "value2"],
+//     leg: "value"
+//   },
+//   indicators: {
+//     temperature: 36.6,
+//     pressure: 1.56,
+//     growth: 178,
+//     weight: 80.78,
+//     sugar: 0.8,
+//     oxygen: 100.1
+//   },
+//   answer: [
+//     {doctor: 0.1},
+//     {doctor: 0.09},
+//     {doctor: 0.08}
+//   ]
+// }
 </script>
 
 <template>
-  <layout>
-    <spinner v-if="isLoading" :class="$style.spinner"></spinner>
+  <Layout :class="$style.container">
+    <Spinner v-if="isLoading" :class="$style.spinner"></Spinner>
     <div v-else>
-      <p class="smed-text_h2 smed-text_medium" :class="$style.title">История обращений</p>
-      <div :class="$style.historyWrapper">
-        <template v-if="history?.length">Не было обращений</template>
-        <template v-else>
-          <div v-html="generateHTML(objectData)"></div>
-        </template>
+      <h2 class="smed-text_h2 smed-text_medium">История</h2>
+      <h3 class="smed-text_h3 smed-text_medium">Основная информация</h3>
+      <p v-for="(value, name) in history.card">{{ func(name, value) }}</p>
+      <h3 class="smed-text_h3 smed-text_medium">Описание жалобы</h3>
+      <p>{{history.inlines_history}}</p>
+      <h3 class="smed-text_h3 smed-text_medium">Дополнительная информация</h3>
+      <p v-for="(value, name) in indicators">{{ translate[name] + " " + value}}</p>
+      <hr/>
+      <div>
+        <h3 class="smed-text_h3 smed-text_medium">Рекомендуемые врачи</h3>
+        <p v-for="doctor in history.answer">{{ doctor.doctor }}</p>
       </div>
-      <base-button :class="$style.button" @click="loadPreviousHistoryItem">Загрузить предыдущее</base-button>
     </div>
-  </layout>
+
+
+
+
+  </Layout>
+<!--  <layout>-->
+<!--    <spinner v-if="isLoading" :class="$style.spinner"></spinner>-->
+<!--    <div v-else>-->
+<!--      <p class="smed-text_h2 smed-text_medium" :class="$style.title">История обращений</p>-->
+<!--      <div :class="$style.historyWrapper">-->
+<!--        <template v-if="history?.length">Не было обращений</template>-->
+<!--        <template v-else>-->
+<!--          <div v-html="generateHTML(objectData)"></div>-->
+<!--        </template>-->
+<!--      </div>-->
+<!--      <base-button :class="$style.button" @click="loadPreviousHistoryItem">Загрузить предыдущее</base-button>-->
+<!--    </div>-->
+<!--  </layout>-->
 </template>
 
 <style module lang="scss">
+.container{
+  text-align: left;
+}
 .spinner {
   position: fixed;
   top: 50%;
